28c11d3e6c1d20050ad0357181c34f43b7175a8d
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-12-13 15:14:02 +0200

    Cleaner comments.py

--- application/comments.py
+++ application/comments.py
@@ -7,14 +7,15 @@ from flask import redirect, render_template, request, session
 def new_comment():
     sudoku_id = request.form["sudoku_id"]
 
+    error = None
     if "user_id" not in session:
-        redirect("/sudoku/" + sudoku_id + "?err=comment_no_user")
+        error = "comment_no_user"
+    elif "csrf_token" not in session or "csrf_token" not in request.form:
+        error = "no_csrf_token"
+    elif session["csrf_token"] != request.form["csrf_token"]:
+        error = "invalid_csrf_token"
 
-    if "csrf_token" not in session or "csrf_token" not in request.form:
-        redirect("/sudoku/" + sudoku_id + "?err=no_csrf_token")
-
-    if session["csrf_token"] != request.form["csrf_token"]:
-        redirect("/sudoku/" + sudoku_id + "?err=invalid_csrf_token")
+    if error: redirect("/sudoku/" + sudoku_id + "?err=" + error)
 
     user_id = session["user_id"]
     rating = request.form["rating"]
