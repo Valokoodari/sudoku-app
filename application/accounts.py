5e8d998b5fc2f401c5d8ccdb50f9f2958f3002b0
Valokoodari <28293838+Valokoodari@users.noreply.github.com>
2020-12-13 14:51:27 +0200

    Fix signup csrf

--- application/accounts.py
+++ application/accounts.py
@@ -18,9 +18,7 @@ def login():
     else:
         password_hash = user[1]
         if check_password_hash(password_hash, password):
-            session["user_id"] = user[0]
-            session["display_name"] = user[2]
-            session["csrf_token"] = os.urandom(16).hex()
+            set_session_details(user)
         else:
             return redirect("/?err=login_incorrect")
 
@@ -28,8 +26,7 @@ def login():
 
 @app.route("/logout")
 def logout():
-    del session["user_id"]
-    del session["display_name"]
+    clear_session_details()
     return redirect("/")
 
 @app.route("/signup", methods=["POST"])
@@ -41,13 +38,22 @@ def signup():
     password_hash = generate_password_hash(password)
 
     error = check_signup(display, username, password, confirm)
-    if error: return render_template("index.html", error=get_msg(error))
+    if error: return redirect("index.html?err=" + error)
 
     user = create_user(username, password_hash, display)
 
     if user:
-        session["user_id"] = user[0]
-        session["display_name"] = user[2]
+        set_session_details(user)
         return redirect("/")
     else:
         return redirect("/?err=signup_db_error")
+
+def set_session_details(user):
+    session["user_id"] = user[0]
+    session["display_name"] = user[2]
+    session["csrf_token"] = os.urandom(16).hex()
+
+def clear_session_details():
+    del session["user_id"]
+    del session["display_name"]
+    del session["csfr_token"]
